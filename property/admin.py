e20f34bc610be224617acf68398f1dd74e1a7b04
Stanislav <yastva3@gmail.com>
2022-11-28 10:58:17 +0600

    Register model via decorator

--- property/admin.py
+++ property/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 from .models import Flat
 
+@admin.register(Flat)
 class AuthorAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner']
     readonly_fields = ['created_at']
 
-admin.site.register(Flat, AuthorAdmin)
+# admin.site.register(Flat, AuthorAdmin)
