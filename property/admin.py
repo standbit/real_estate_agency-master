de59b0939e9292cf89d1f4480f0b9c412d09f474
Stanislav <yastva3@gmail.com>
2022-11-30 12:46:52 +0600

    Add db model for complaints

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import Flat
+from .models import Complaint
 
 @admin.register(Flat)
 class AuthorAdmin(admin.ModelAdmin):
@@ -9,3 +10,8 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town',)
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
+    raw_id_fields = ()
+
+@admin.register(Complaint)
+class AuthorAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user', 'flat')
--- property/models.py
+++ property/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 BUILDING_TYPES = ((True, 'Да'), (False, 'Нет'), (None, "Неизвестно"))
 
@@ -52,3 +53,20 @@ class Flat(models.Model):
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+
+class Complaint(models.Model):
+    user = models.ForeignKey(
+        User,
+        verbose_name='Кто жаловался:',
+        null=True,
+        on_delete=models.SET_NULL)
+    flat = models.ForeignKey(
+        Flat,
+        verbose_name='Квартира, на которую пожаловались:',
+        null=True,
+        on_delete=models.SET_NULL)
+    text = models.TextField(
+        verbose_name='Текст жалобы:',
+        null=True
+    )
